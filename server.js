b02026ab59094fd67ab1f798c9b6323322cfb192
Lev Eidelman Nagar <lev@techsee.me>
2015-10-21 10:52:22 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+
+/*=================================
+=            server.js            =
+=================================*/
+
+var redis  = require('redis');
+var client = redis.createClient(6379, "127.0.0.1");
+
+
